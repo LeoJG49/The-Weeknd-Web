c307be128f54bbb657863eead1222cb6b6f7ed12
Leo Dev <leoneljovel49@gmail.com>
2026-02-03 22:51:46 +0100

    Add responsive behavior to horizontal scroll animation with mobile text animation fallback

--- JavaScript/horizontalScrollTitleGSAP.js
+++ JavaScript/horizontalScrollTitleGSAP.js
@@ -1,16 +1,42 @@
-gsap.registerPlugin(ScrollTrigger);
+if (window.matchMedia('(min-width: 1200px)').matches) {
+    gsap.registerPlugin(ScrollTrigger);
+    let horizontal = gsap.to(".about-wrapper", {
+        x: "-480vw",
+        ease: "none",
+        paused: true
+    });
+    
+    ScrollTrigger.create({
+        trigger: ".about-wrapper",
+        start: "top top",
+        end: "+=900vh",
+        scrub: 1,
+        pin: true,
+        animation: horizontal
+    });
+}
 
-let horizontal = gsap.to(".about-wrapper", {
-    x: "-480vw",
-    ease: "none",
-    paused: true
-});
+if (window.matchMedia('(max-width: 1199px)').matches) {
+    document.fonts.ready.then(() => {
+    //About The Weeknd Text Animation
+    gsap.set(".about-title", { opacity: 1});
 
-ScrollTrigger.create({
-    trigger: ".about-wrapper",
-    start: "top top",
-    end: "+=900vh",
-    scrub: 1,
-    pin: true,
-    animation: horizontal
-});
+    let split;
+    SplitText.create(".about-title", {
+        type: "words, lines, chars",
+        wordsClass: "word",
+        linesClass: "line",
+        charsClass: "char",
+        autoSplit: true,
+        mask: "words" ,
+        onSplit: (self) => {
+            split = gsap.from(self.words, {
+                y: 150,
+                ease: "power2.out",
+                stagger: .2,
+            })
+            return split;
+        }
+    })
+})
+}
